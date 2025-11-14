db9486cdfdaca0a97c9862e3f3ffbc4f1db0b8d6
Hugh Nimmo-Smith <hughns@element.io>
2025-11-14 11:21:17 +0000

    Prototype implementation of MSC4108 v2025

--- crates/ruma-client-api/src/rendezvous.rs
+++ crates/ruma-client-api/src/rendezvous.rs
@@ -1,3 +1,6 @@
 //! Endpoints for managing rendezvous sessions.
 
 pub mod create_rendezvous_session;
+pub mod get_rendezvous_session;
+pub mod update_rendezvous_session;
+pub mod delete_rendezvous_session;
---
+++ crates/ruma-client-api/src/rendezvous/get_rendezvous_session.rs
@@ -0,0 +1,55 @@
+//! `GET /_matrix/client/*/rendezvous/{id}`
+//!
+//! Get a rendezvous session.
+
+pub mod unstable {
+    //! `msc4108` ([MSC])
+    //!
+    //! [MSC]: https://github.com/matrix-org/matrix-spec-proposals/pull/4108
+
+    use ruma_common::{
+        api::{auth_scheme::NoAuthentication, request, response},
+        metadata,
+    };
+
+    metadata! {
+        method: GET,
+        rate_limited: false,
+        authentication: NoAuthentication,
+        history: {
+            unstable("io.element.msc4108") => "/_matrix/client/unstable/io.element.msc4108/rendezvous/{id}",
+        }
+    }
+
+    /// Request type for the `GET` `rendezvous` endpoint.
+    #[request(error = crate::Error)]
+    pub struct Request {
+        /// The ID of the rendezvous session to get.
+        #[ruma_api(path)]
+        pub id: String,
+    }
+
+    impl Request {
+        /// Creates a new `Request` with the given id.
+        pub fn new(id: String) -> Self {
+            Self { id }
+        }
+    }
+
+    #[response(error = crate::Error)]
+    /// Response type for the `GET` `rendezvous` endpoint.
+    pub struct Response {
+        /// The current sequence token for the session.
+        pub sequence_token: String,
+
+        /// The current data for the session.
+        pub data: String,
+    }
+
+    impl Response {
+        /// Creates a new `Response` with the given sequence token and data.
+        pub fn new(sequence_token: String, data: String) -> Self {
+            Self { sequence_token, data }
+        }
+    }
+}
